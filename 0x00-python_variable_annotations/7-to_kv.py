4a10e815ae9f0bc0edf571769480eb35a4c6407c
Martin Corredor <dev.martincorredor@gmail.com>
2021-10-18 14:22:10 -0500

    |FIXED| Fix the sintax errors

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Python Module"""
-from typing import Union, Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v:Union[int, float]) -> Tuple[str, float]:
-    """Takes a string and integer or float and returns a tuple"""
-    return(k, v*v)
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """return a float sum"""
+    return (k, v*v)
