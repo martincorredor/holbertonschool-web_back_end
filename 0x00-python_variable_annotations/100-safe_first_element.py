4363b420a9963edf738ce8f0cc14746dc0472d8b
Martin Corredor <dev.martincorredor@gmail.com>
2021-10-18 14:32:10 -0500

    |ADDED| Add advanced tasks

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 """Python Module"""
+
+
+def safe_first_element(lst):
+    if lst:
+        return lst[0]
+    else:
+        return None
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 """Python Module"""
+
+
+def safely_get_value(dct, key, default = None):
+    if key in dct:
+        return dct[key]
+    else:
+        return default
